bac1cbd078555ea3bfb7b780943ae3c53cbd278b
kkangh00n <kig2454@gmail.com>
2024-10-04 01:00:36 +0900

    가장 긴 증가하는 부분 수열

---
+++ src/dp/BOJ_11053.java
@@ -0,0 +1,37 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11053 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        int[] dp = new int[N];
+
+        // 수열 초기화
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        //LIS (최장 증가 부분 수열)
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+
+    }
+}
