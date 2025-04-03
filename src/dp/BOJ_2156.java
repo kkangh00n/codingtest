8c63228a9e415fa8664876ebdd38df4681752ba7
kkangh00n <kig2454@gmail.com>
2025-04-03 14:57:55 +0900

    포도주 시식 (설명 추가)

--- src/dp/BOJ_2156.java
+++ src/dp/BOJ_2156.java
@@ -4,6 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 포도주를 연속으로 3잔을 마실 수 없다.
+ * 많은 양의 포도주를 마시고 싶다.
+ *
+ * dp 배열
+ * i: 해당 자리
+ * dp[i]: 해당 자리에 있을 때의 마신 최대 포도주 양
+ * a. 포도주를 세 잔 연속으로 마신 경우 -> dp[i-1]
+ * b. 포도주를 두 잔 마시고 한 잔 건너뛰고 마신 경우, -> dp[i-2] + arr[i]
+ * c. 포도주를 두 잔 마시고 두 잔 건너뛰고 마신 경우, -> dp[i-3] + arr[i-1] + arr[i]
+ */
 public class BOJ_2156 {
 
     public static void main(String[] args) throws IOException {
