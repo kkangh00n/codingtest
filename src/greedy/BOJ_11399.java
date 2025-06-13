a51ed1552db4e6ddec73584aee3606fdb9167e3c
kkang_h00n <kig2454@gmail.com>
2025-06-13 11:28:06 +0900

    ATM

---
+++ src/greedy/BOJ_11399.java
@@ -0,0 +1,35 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11399 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        int answer = 0;
+        int temp = 0;
+
+        for (int i = 0; i < N; i++) {
+            temp += nums[i];
+            answer += temp;
+        }
+
+        System.out.println(answer);
+    }
+}
